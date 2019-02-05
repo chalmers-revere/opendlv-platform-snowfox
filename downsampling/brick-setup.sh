4ba25254cbfe97d23e1db53389b2bbc3a2cf4431
Christian Berger <christian.berger@gu.se>
2019-02-05 13:29:16 +0100

    * Brick setup
    Signed-off-by: Christian Berger <christian.berger@gu.se>

---
+++ downsampling/brick-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo ethtool --set-eee enp10s0 eee off
+sudo ip link set lo multicast on
+sudo ip link set enp10s0 multicast on
+sudo route add -net 224.0.0.0 netmask 240.0.0.0 dev enp10s0
+sudo route add -net 224.0.0.0 netmask 240.0.0.0 dev lo
+sudo route add -net 225.0.0.111 netmask 255.255.255.255 dev lo
