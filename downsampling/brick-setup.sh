f66b903e03fe32cdc24df0394e5820fae1d9f2e6
Christian Berger <christian.berger@gu.se>
2019-02-05 14:21:58 +0100

    * renaming the pipes
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- downsampling/brick-setup.sh
+++ downsampling/brick-setup.sh
@@ -13,4 +13,5 @@ sudo ip link set enp11s0 multicast off
 
 sudo route add -net 224.0.0.0 netmask 240.0.0.0 dev enp10s0
 sudo route add -net 224.0.0.0 netmask 240.0.0.0 dev lo
+sudo route add -net 225.0.0.109 netmask 255.255.255.255 dev enp10s0
 sudo route add -net 225.0.0.111 netmask 255.255.255.255 dev lo
