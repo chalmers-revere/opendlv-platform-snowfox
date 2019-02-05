bcf10ba9e55d1c356afc0caa5c57cb8a40470d8e
Christian Berger <christian.berger@gu.se>
2019-02-05 13:34:18 +0100

    * eee changes
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- downsampling/brick-setup.sh
+++ downsampling/brick-setup.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+sudo ethtool --set-eee enp4s0 eee off
+sudo ethtool --set-eee enp5s0 eee off
 sudo ethtool --set-eee enp10s0 eee off
+sudo ethtool --set-eee enp11s0 eee off
+
 sudo ip link set lo multicast on
 sudo ip link set enp10s0 multicast on
+
 sudo route add -net 224.0.0.0 netmask 240.0.0.0 dev enp10s0
 sudo route add -net 224.0.0.0 netmask 240.0.0.0 dev lo
 sudo route add -net 225.0.0.111 netmask 255.255.255.255 dev lo
