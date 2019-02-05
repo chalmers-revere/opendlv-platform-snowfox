9d4a6c1a8625fbfe42767b377ae650c9e43b007d
Christian Berger <christian.berger@gu.se>
2019-02-05 13:49:49 +0100

    * routes and multicast
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- downsampling/brick-setup.sh
+++ downsampling/brick-setup.sh
@@ -6,7 +6,10 @@ sudo ethtool --set-eee enp10s0 eee off
 sudo ethtool --set-eee enp11s0 eee off
 
 sudo ip link set lo multicast on
+sudo ip link set enp4s0 multicast off
+sudo ip link set enp5s0 multicast off
 sudo ip link set enp10s0 multicast on
+sudo ip link set enp11s0 multicast off
 
 sudo route add -net 224.0.0.0 netmask 240.0.0.0 dev enp10s0
 sudo route add -net 224.0.0.0 netmask 240.0.0.0 dev lo
