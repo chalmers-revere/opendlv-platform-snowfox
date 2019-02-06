50d771713d141a58c02b666912986823f9f75473
Christian Berger <christian.berger@gu.se>
2019-02-06 13:50:28 +0100

    * monitoring
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- downsampling/monitor-sysload.sh
+++ downsampling/monitor-sysload.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-for i in 1 2 3 4 5 6 7; do ps -eo psr,pcpu,command | tr -s " " | grep "^ [$i]" | grep -v "\]"; done
+for i in 1 2 3 4 5 6 7; do
+    ts=$(date +%s%N)
+    sysload=$(ps -eo psr,pcpu,command | tr -s " " " " | grep "^ [$i]" | grep -v "\]" 2>&1 | sed -e "s/\ /\;/" | sed -e "s/\ /\;/" | sed -e "s/\ /\;/")
+    echo "${sysload}" | sed -e "s/^;/${ts}\;/"
+done
 
