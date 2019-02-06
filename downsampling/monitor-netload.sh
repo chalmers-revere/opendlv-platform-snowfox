83a4c1533a41cfa39b3722e457fa6bcfdd13a486
Christian Berger <christian.berger@gu.se>
2019-02-06 21:31:11 +0100

    * Added plotting
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- downsampling/monitor-netload.sh
+++ downsampling/monitor-netload.sh
@@ -1,5 +1,47 @@
-#!/bin/sh
+#!/bin/bash
 
-netload=$(bmon -p enp10s0,enp11s0,enp5s0,enp4s0 -o 'format:fmt=$(element:name)%RX:%$(attr:rxrate:bytes)%TX:%$(attr:txrate:bytes)\n;quitafter=2')
-ts=$(date +%s%N)
-echo "${netload}" | sed -e "s/enp/${ts};enp/" | sed -e "s/%/\;/g" | grep -v "RX:;0.00;TX:;0.00"
+RUNNING=1
+trap 'cleanup' TERM
+
+cleanup() {
+    echo "Cleaning up..."
+    RUNNING=0
+}
+
+FILENAME=$(date +%F_%R%S)-netload.csv
+FILENAME=$(echo ${FILENAME} | sed -e 's/\://g')
+:> ${FILENAME}
+
+while [ "$RUNNING" == "1" ]; do
+    netload=$(bmon -p $@ -o 'format:fmt=$(element:name)%RX:%$(attr:rxrate:bytes)%TX:%$(attr:txrate:bytes)\n;quitafter=2')
+    ts=$(date +%s%N)
+    echo "${netload}" | sed -e "s/enp/${ts};enp/" | sed -e "s/%/\;/g" | grep -v "RX:;0.00;TX:;0.00" | tee -a ${FILENAME}
+
+    sleep 1
+done
+
+for i in $@ ; do
+    MATCH="/.*;$i;.*/!d"
+    cat ${FILENAME} | sed -e ${MATCH} > ${FILENAME}-${i}
+    PROGRAM=$(cat ${FILENAME}-${i} |head -1|cut -f4 -d";")
+    TITLE=$(echo ${FILENAME} | sed -e 's/_/-/')
+cat <<EOF >netload.gnuplot
+#set terminal pngcairo  transparent enhanced font "arial,10" fontscale 1.0 size 600, 400 
+set terminal pdf
+set output '${FILENAME}-${i}.pdf'
+set style increment default
+set title "${TITLE}/$i"
+set title  font ",20" norotate
+set xrange [ * : * ] noreverse writeback
+set yrange [ 0 : * ] noreverse writeback
+set datafile separator ";"
+show style line
+plot '${FILENAME}-${i}' using 0:4 with line lt 1 lc 3 title 'RX', \
+     '${FILENAME}-${i}' using 0:6 with line lt 1 lc 4 title 'TX'
+EOF
+cat netload.gnuplot | gnuplot
+rm -f ${FILENAME}-${i}
+done
+
+rm -f netload.gnuplot
+
--- downsampling/monitor-sysload.sh
+++ downsampling/monitor-sysload.sh
@@ -1,8 +1,48 @@
-#!/bin/sh
+#!/bin/bash
 
-for i in 1 2 3 4 5 6 7; do
-    ts=$(date +%s%N)
-    sysload=$(ps -eo psr,pcpu,command | tr -s " " " " | grep "^ [$i]" | grep -v "\]" 2>&1 | sed -e "s/\ /\;/" | sed -e "s/\ /\;/" | sed -e "s/\ /\;/")
-    echo "${sysload}" | sed -e "s/^;/${ts}\;/"
+RUNNING=1
+trap 'cleanup' TERM
+
+cleanup() {
+    echo "Cleaning up..."
+    RUNNING=0
+}
+
+FILENAME=$(date +%F_%R%S)-sysload.csv
+FILENAME=$(echo ${FILENAME} | sed -e 's/\://g')
+:> ${FILENAME}
+
+while [ "$RUNNING" == "1" ]; do
+    for i in $@ ; do
+        ts=$(date +%s%N)
+        sysload=$(ps -eo psr,pcpu,command | tr -s " " " " | grep "^ [$i]" | grep -v "\]" 2>&1 | sed -e "s/\ /\;/" | sed -e "s/\ /\;/" | sed -e "s/\ /\;/" | head -1)
+        echo "${sysload}" | sed -e "s/^;/${ts}\;/" | tee -a ${FILENAME}
+    done
+
+    sleep 1;
 done
+
+for i in $@ ; do
+    MATCH="/.*;$i;.*/!d"
+    cat ${FILENAME} | sed -e ${MATCH} > ${FILENAME}.core-${i}
+    PROGRAM=$(cat ${FILENAME}.core-${i} |head -1|cut -f4 -d";")
+    TITLE=$(echo ${FILENAME} | sed -e 's/_/-/')
+cat <<EOF >sysload.gnuplot
+#set terminal pngcairo  transparent enhanced font "arial,10" fontscale 1.0 size 600, 400 
+set terminal pdf
+set output '${FILENAME}.core-${i}.pdf'
+set style increment default
+set title "${TITLE}/core $i"
+set title  font ",20" norotate
+set xrange [ * : * ] noreverse writeback
+set yrange [ 0 : 100 ] noreverse writeback
+set datafile separator ";"
+show style line
+plot '${FILENAME}.core-${i}' using 0:3 with line lt 1 lc 3 title '${PROGRAM}'
+EOF
+cat sysload.gnuplot | gnuplot
+rm -f ${FILENAME}.core-${i}
+done
+
+rm -f sysload.gnuplot
 
