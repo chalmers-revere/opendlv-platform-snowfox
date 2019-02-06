540d3f459e8541be615b1b79680aeab033265940
Christian Berger <christian.berger@gu.se>
2019-02-06 13:55:51 +0100

    * monitoring netload
    Signed-off-by: Christian Berger <christian.berger@gu.se>

--- downsampling/monitor-netload.sh
+++ downsampling/monitor-netload.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-bmon -p enp10s0,enp11s0,enp5s0,enp4s0 -o 'format:fmt=$(element:name)\tRX: $(attr:rxrate:bytes)\tTX: $(attr:txrate:bytes)\n;quitafter=2'
+netload=$(bmon -p enp10s0,enp11s0,enp5s0,enp4s0 -o 'format:fmt=$(element:name)%RX:%$(attr:rxrate:bytes)%TX:%$(attr:txrate:bytes)\n;quitafter=2')
+ts=$(date +%s%N)
+echo "${netload}" | sed -e "s/enp/${ts};enp/" | sed -e "s/%/\;/g" | grep -v "RX:;0.00;TX:;0.00"
